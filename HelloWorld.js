3d2c1e028cd2ad8a4878aad3d936b5da4f074ea1
Jose Hernandez <puch007@mail.fresnostate.edu>
2019-08-29 16:29:25 -0700

    make code better

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,8 +1,8 @@
 var HelloWorld = (function () {
     function HelloWorld(firstName, lastName) {
-        this.firstName = '';
         this.firstName = firstName;
         this.lastName = lastName;
+        this.firstName = '';
     }
     HelloWorld.prototype.sayhello = function () {
         console.log('Hello World!' + 'My name is' + this.firstName + ' ' + this.lastName);
